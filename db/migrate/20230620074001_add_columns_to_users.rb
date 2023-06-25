03b39cdd9358fdeebeaff942da84266365f39413
kota0987890 <114126096+kota0987890@users.noreply.github.com>
2023-06-25 18:46:26 +0900

    Revert "mvp"

--- db/migrate/20230620074001_add_columns_to_users.rb
+++
@@ -1,8 +0,0 @@
-class AddColumnsToUsers < ActiveRecord::Migration[5.2]
-  def change
-    add_column :users, :token, :string
-    add_column :users, :refresh_token, :string
-    add_column :users, :expires_at, :integer
-    add_column :users, :expires, :boolean
-  end
-end
--- db/migrate/20230625072715_remove_token_from_users.rb
+++
@@ -1,8 +0,0 @@
-class RemoveTokenFromUsers < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :users, :token, :string
-    remove_column :users, :refresh_token, :string
-    remove_column :users, :expires_at, :integer
-    remove_column :users, :expires, :boolean
-  end
-end
